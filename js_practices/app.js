1700be655bdbcddbcb17a5e2d1ac3944047d1d17
Betul <betulcil.25@gmail.com>
2023-09-28 21:30:43 +0300

    rem

---
+++ js_practices/app.js
@@ -0,0 +1,13 @@
+//*Cmments 
+//? welcome to 30 days of javascript
+
+let betul= "merhaba",
+duzenli= 3,
+clas;
+console.log(betul, duzenli, clas)
+console.log(typeof (betul, duzenli, clas, betul)) 
+
+let my_age=33
+let your_age=40
+console.log(`Benim yaşım ${my_age} senin yaşın ${your_age}`);
+
