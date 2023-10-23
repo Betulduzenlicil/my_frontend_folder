ddb116a06a54827aa7e7ece4ae4e428247b53647
Betul <betulcil.25@gmail.com>
2023-10-23 21:43:42 +0300

    object

---
+++ js_inclass_10/objects.js
@@ -0,0 +1,39 @@
+console.log("OBJECTS");
+
+// cars=["Audio", "BMW", "Anadol"]
+// brand=["0.8", "3.20", "lux"]
+// engine=[4000, 2000, 1.600]
+// speed=[8,7,4]
+// power=["hybrid", "electrity", "fuel+lpg"]
+
+// const cars={
+//     car: "Audi",
+//     brand:"Q8",
+//     engine:4000,
+//     speed: 8
+
+// }
+
+
+const cars=["Audi","BMW","Anadol"]
+const brand= ["Q8","3 20", "Lux"]
+const engine=[4000,2000,1600]
+const speed=[8,7,4]
+const power=["hybrid","electricity","Fuel+LPG"]
+ console.log(cars[0], brand[0], engine[0], speed[0]);
+
+
+
+
+ const araba={
+    car: "Audi",
+    brand:"Q8",
+    engine:4000,
+    speed: 8
+
+ }
+ console.log(araba.car);
+ console.log(araba["car"]);
+ console.log(araba.brand);
+ console.log(araba.engine);
+ console.log(araba.car)
